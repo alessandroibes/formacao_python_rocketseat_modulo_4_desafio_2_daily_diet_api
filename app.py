9c3531137ec75b6b618871fa60a56076da348c4e
alessandroibes <alessandro.ibes@gmail.com>
2025-04-26 13:45:24 -0300

    Add authentication with Flask-Login

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
+from authentication import login_manager
 from database import db
+from models.user import User
 
 
 app = Flask(__name__)
@@ -10,6 +12,22 @@ app.config["SECRET_KEY"] = "my_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
+login_manager.init_app(app)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return jsonify({ "message": "Autenticação realizada com sucesso" })
+        
+    return jsonify({ "message": "Credenciais inválidas" }), 400
 
 
 @app.route("/", methods=["GET"])
